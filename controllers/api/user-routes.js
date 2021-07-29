4faced750af671929a5b351d17c2edf08551465e
mtpullin <mtpullin@gmail.com>
2021-07-29 19:46:17 -0400

    added login logout routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const {User, Recipie, Comment} = require('../../models')
+const Auth = require('../../utils/auth')
+
+router.post('/login', Auth, (req,res)=> {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData){
+            res.status(400).json({message: 'no user with that email'})
+            return;
+        }
+
+        const validPass = dbUserData.checkpass(req.body.password)
+
+        if(!validPass) {
+            res.status(400).json({message: 'incorrect password'})
+            return
+        }
+        req.session.save(()=> {
+            req.session.user_id = dbUserData.user_id
+            req.session.username = dbUserData.username
+            req.session.loggedIn = true
+
+            res.json({user: dbUserData, message: 'Logged in!'})
+        })
+    })
+})
+
+
+router.post('/logout', Auth, (req,res)=> {
+    if(req,session.loggedIn){
+        req.session.destroy(()=> {
+            res.status(204).end()
+        })
+    }else {
+        res.status(404).end()
+    }
+})
