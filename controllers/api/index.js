aaa7c558ca9786b99bdc2a7727ef6fd9f8689db0
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-08-01 13:51:40 -0400

    Preliminary api calls to youtube api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const testRoutes = require('./test-routes.js');
 const userRoutes = require('./user-routes')
+const ytRoutes = require('./youtube-routes')
+
+router.use('/yt', ytRoutes)
 router.use('/test', testRoutes);
 router.use('/login', userRoutes)
+
 module.exports = router;
