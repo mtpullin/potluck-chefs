cea5d1e08869ee56c3a80d5716e17b73231145be
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-08-01 22:34:23 -0400

    changed database name

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -3,9 +3,6 @@ async function helloWorld() {
 }
 
 helloWorld()
-const api =  fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=pikachu&type=video&key=AIzaSyDzQD4C9xqP-BI3gizn-_eltKiS0GKu41o').then(data => {
-    return console.log(data.json())
-})
-api
 
-// https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=pikachu&type=video&key=AIzaSyDzQD4C9xqP-BI3gizn-_eltKiS0GKu41o
+
+// https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=pikachu&type=video&key=
