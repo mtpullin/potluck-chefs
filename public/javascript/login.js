a2e8677d40a95c64ed430856ff8c4d388e8e04a5
mtpullin <mtpullin@gmail.com>
2021-08-02 17:26:36 -0400

    finished login and signup form functions

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -28,4 +28,25 @@ async function signupForm(event) {
     const username = document.querySelector('#username-signup').value.trim()
     const email = document.querySelector('#email-signup').value.trim()
     const password = document.querySelector('#password-signup').value.trim()
+
+    if(username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {'Content-type':'application/json'}
+        })
+        if(response.ok){
+            document.location.replace('/')
+        } else {
+            alert(response.statusText)
+        }
+    }
 }
+
+document.querySelector('.login').addEventListener('submit', loginForm)
+
+document.querySelector('.signup').addEventListener('submit', signupForm)
