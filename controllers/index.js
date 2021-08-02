f8a9b41dd8b9630088420f6122d89cd35434510e
mtpullin <mtpullin@gmail.com>
2021-08-02 19:48:49 -0400

    remade kitchen route

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const loginroutes = require('./loginroutes')
 const apiRoutes = require('./api/');
 const indexRoutes = require('./index-routes')
-const myKitchenRoutes = require('./mykitchen-routes')
+const kitchenroutes = require('./kitchenroutes')
 
-router.use('/mykitchen', myKitchenRoutes)
+router.use('/kitchen', kitchenroutes)
 router.use('/login', loginroutes)
 router.use('/api', apiRoutes);
 router.use('/', indexRoutes);
--- controllers/kitchenroutes.js
+++ controllers/kitchenroutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 router.get('/', (req,res)=> {
-    res.render('mykitchen')
+    res.render('kitchen')
 })
 module.exports = router
