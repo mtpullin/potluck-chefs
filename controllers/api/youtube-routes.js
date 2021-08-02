7bc49d715b16d7710fe7a7e646b846944eeda6cf
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-08-01 22:16:58 -0400

    tested different api route

--- controllers/api/youtube-routes.js
+++ controllers/api/youtube-routes.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 
 router.get('/', (req,res) => {
-    axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=pikachu&type=video&key=${process.env.YT_API}`)
+    axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=chicken_pot_pie&type=video&key=${process.env.YT_API}`)
       .then(response=> {
 
         return  res.send(response.data)
