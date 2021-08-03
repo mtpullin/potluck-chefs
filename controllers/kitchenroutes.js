7ebc39b86ba9236de3422d52746a29df80826c00
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-08-03 14:25:39 -0400

    ingredients and steps fully destructured

--- controllers/kitchenroutes.js
+++ controllers/kitchenroutes.js
@@ -8,15 +8,15 @@ router.get('/', (req,res)=> {
         stepsArr = []
         recipe= []
        data.forEach(element => {
-        recipe.push({name:element.name,ingredients:element.ingredients.split(','), amount:element.amounts.split(','),stepObj:element.steps.split(',')})
+        recipe.push({name:element.name,ingredients:[{itemIngredient:element.ingredients.split(',')}],amount:[{itemAmount:element.amounts.split(',')}],steps:[{itemStep:element.steps.split(',')}]})
         names.push({name:element.name})
         ingredientArr.push({ingredients:element.ingredients.split(','), amount:element.amounts.split(',')})
         stepsArr.push({stepObj:element.steps.split(',')})
        })
+    //    console.log(ingredientArr)
+        //    console.log(names,ingredientArr,stepsArr)
 
-       console.log(names,ingredientArr,stepsArr)
-       console.log(recipe)
-       res.render('kitchen', {recipe:recipe})
+       res.render('kitchen', {recipe:recipe })
     })
 })
 module.exports = router
