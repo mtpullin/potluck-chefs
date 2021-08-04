e41b36eecb4337e3bf67f4bc863d083406e44a75
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-08-04 14:03:39 -0400

    Quick fix to fix issue of dead CDN from materialize

--- controllers/kitchenroutes.js
+++ controllers/kitchenroutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const e = require('express');
 const { User, Recipe } = require('../models')
 const Auth = require('../utils/auth')
 router.get('/', Auth, (req, res) => {
     Recipe.findAll().then(data => {
-        recipe = []
+        var recipe=[]
         data.forEach(element => {
             recipe.push({ name: element.name, ingredients: [{ itemIngredient: element.ingredients.split(',') }], amount: [{ itemAmount: element.amounts.split(',') }], steps: [{ itemStep: element.steps.split(',') }] })
         })
