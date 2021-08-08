8d5ab635514fafdb2af85039d80bbf4d9a8ad5e8
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-08-08 00:19:21 -0400

    Mobile sidebar cssed

--- controllers/index-routes.js
+++ controllers/index-routes.js
@@ -1,20 +1,30 @@
-const router = require('express').Router();3
-const {User, Recipe} = require('../models')
+const router = require('express').Router();
+const { User, Recipe } = require('../models')
+
 router.get('/index', (req, res) => {
+    recipe = []
     Recipe.findAll().then(data => {
-        recipe = []
         data.forEach(element => {
-            recipe.push({id:element.id, name: element.name, ingredients: [{ itemIngredient: element.ingredients.split(',') }], amount: [{ itemAmount: element.amounts.split(',') }], steps: [{ itemStep: element.steps.split(',') }], videoLink: element.videoLink, videoImage:element.videoImage })
+            recipe.push({ id: element.id, name: element.name, ingredients: [{ itemIngredient: element.ingredients.split(',') }], amount: [{ itemAmount: element.amounts.split(',') }], steps: [{ itemStep: element.steps.split(',') }], videoLink: element.videoLink, videoImage: element.videoImage })
         })
-        if(req.session.loggedIn)
-        res.render('index', { recipe: recipe, loggedIn: true })
-        else 
-        res.render('index', { recipe: recipe})
+        if (req.session.loggedIn)
+        User.findAll({
+            where: {
+                id: req.session.user_id
+            }
+        }).then(data => {
+            userInfo = []
+            userInfo.push({ username: data[0].username, email: data[0].email })
+            return res.render('index', {userInfo, recipe: recipe, loggedIn: true })
+        })
+        else
+                return res.render('index', {recipe: recipe })
     })
 })
 
-router.get('/',(req,res) => {
+
+router.get('/', (req, res) => {
     res.redirect('/index');
 })
 
-module.exports=router
+module.exports = router
--- controllers/kitchenroutes.js
+++ controllers/kitchenroutes.js
@@ -1,22 +1,28 @@
 const router = require('express').Router();
-const e = require('express');
 const { User, Recipe } = require('../models')
 const Auth = require('../utils/auth')
 router.get('/', Auth, (req, res) => {
+    var recipe = []
     Recipe.findAll({
-        where:{
+        where: {
             user_id: req.session.user_id
         }
     }).then(data => {
-        var recipe=[]
         data.forEach(element => {
-            recipe.push({id:element.id, name: element.name, ingredients: [{ itemIngredient: element.ingredients.split(',') }], amount: [{ itemAmount: element.amounts.split(',') }], steps: [{ itemStep: element.steps.split(',') }], videoLink: element.videoLink, videoImage:element.videoImage  })
+            recipe.push({ id: element.id, name: element.name, ingredients: [{ itemIngredient: element.ingredients.split(',') }], amount: [{ itemAmount: element.amounts.split(',') }], steps: [{ itemStep: element.steps.split(',') }], videoLink: element.videoLink, videoImage: element.videoImage })
         })
-
-        if(req.session.loggedIn)
-        res.render('kitchen', { recipe: recipe, loggedIn: true })
-        else 
-        res.render('kitchen', { recipe: recipe})
+        User.findAll({
+            where: {
+                id: req.session.user_id
+            }
+        }).then(data => {
+            userInfo = []
+            userInfo.push({ username: data[0].username, email: data[0].email })
+            if (req.session.loggedIn)
+                return res.render('kitchen', { userInfo, recipe: recipe, loggedIn: true })
+            else
+                return res.render('kitchen', { userInfo, recipe: recipe })
+        })
     })
 })
 module.exports = router
